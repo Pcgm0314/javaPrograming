873a959fcd7545427c7bea4a501e14d874d61d2c
admin <admin@DESKTOP-4RMT5GG>
2024-05-23 12:54:42 +0900

    .

--- javaPrograming/src/week10/ScoreAverage.java
+++ javaPrograming/src/week10/ScoreAverage.java
@@ -8,7 +8,8 @@ public class ScoreAverage {
 				{3.5, 3.6},
 				{3.7,4.0},
 				{4.1, 4.2}};
-		double sum=0;
+		double sum=0; //double 소수점
+		
 		for(int year = 0; year<score.length; year++) {//각 학년 별로 반복 3
 			for(int term=0;term<score[year].length;term++) {//각 학년의 학기별로 반복 2
 				sum += score[year][term];//전체 평점 합
